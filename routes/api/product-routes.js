ce33db80cbc9177d5d00bedf2ce77d46e7968159
JelenaTomic <tomic.elena09@gmail.com>
2022-09-07 17:06:53 -0500

    find by id  products in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,14 +9,7 @@ router.get('/', (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [
-        {
-          model: Category,
-        },
-        {
-          model: Tag,
-        }
-      ]
+      include: [{model: Category},{model: Tag}]
     });
     res.status(200).json(productData);
   } 
@@ -29,6 +22,20 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  } 
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
