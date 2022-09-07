67ed46937de46f2219d3105aa4aaa8ad0e6add9e
JelenaTomic <tomic.elena09@gmail.com>
2022-09-07 17:14:38 -0500

    delete one product by id in product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -48,7 +48,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+    Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -112,8 +112,17 @@ router.put('/:id', (req, res) => {
     });
 });
 
+
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where:
+    { id:req.params.id }
+  })
+  .then((data) => res.json(data))
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
